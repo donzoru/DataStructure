ad9995b32a75f09566aff79408ebf5d5157bb5df
ドン・ゾル <donzoru@gmail.com>
2018-12-14 22:32:00 +0800

    Update inorder.cpp

--- threadedBinaryTree/inorder.cpp
+++ threadedBinaryTree/inorder.cpp
@@ -4,14 +4,16 @@ const int M = 10;
 int t[M];
 int l[M],r[M];
 int lt[M],rt[M];
-void init(){
+void init()
+{
     srand(time(NULL));
     cout<<"             ";
     for(int i=1;i<M;++i)
         cout<<i<<' ';
     cout<<endl;
     cout<<"tree array : ";
-    for(int i=1;i<M;++i){
+    for(int i=1;i<M;++i)
+    {
         t[i]=rand()%100;
         if(i*2<M) l[i] = i*2,lt[i] = 0;
         else l[i] = 0, lt[i] = 1;
@@ -22,16 +24,21 @@ void init(){
     }
     cout<<endl;
 }
-void inorderWithout(){
+
+void inorderWithout()
+{
     cout<<"Unthreaded Inorder Traversal : ";
     stack<int> s;
     int p = 1;
-    while(s.size() || p){
-        while(p){
+    while(s.size() || p)
+    {
+        while(p)
+        {
             s.push(p);
             p = l[p];
         }
-        if(s.size()){
+        if(s.size())
+        {
             p = s.top(); s.pop();
             cout<<t[p]<<' ';
             p = r[p];
@@ -39,7 +46,9 @@ void inorderWithout(){
     }
     cout<<endl;
 }
-void inThread(int p,int &pre){
+
+void inThread(int p,int &pre)
+{
     if(p){
         inThread(l[p],pre);
         if(l[p]==0){
@@ -57,29 +66,38 @@ void inThread(int p,int &pre){
 void creatInThread(int p)
 {
     int pre = 0;
-    if(p){
+    if(p)
+    {
         inThread(p,pre);
         r[pre] = 0;
         rt[pre] = 1;
     }
 }
-int First(int p){
+
+int First(int p)
+{
     while(lt[p]==0)
         p = l[p];
     return p;
 }
-int Next(int p){
+
+int Next(int p)
+{
     if(rt[p]==0)
         return First(r[p]);
     return r[p];
 }
-void inorderWith(){
+
+void inorderWith()
+{
     cout<<"Threaded Inorder Traversal :   ";
     for(int p = First(1);p;p=Next(p))
         cout<<t[p]<<' ';
     cout<<endl;
 }
-int main(){
+
+int main()
+{
     init();
     inorderWithout();
     creatInThread(1);
